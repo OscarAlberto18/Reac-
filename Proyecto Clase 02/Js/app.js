d1732ce39100fd66607e02bbf073b8851807024b
Oscar Alberto 18 <ibanez0675@gmail.com>
2023-03-08 04:25:27 -0300

    unos cambios

--- Proyecto Clase 02/Js/app.js
+++ Proyecto Clase 02/Js/app.js
@@ -1,5 +1,12 @@
-alert("Bievenido")
 
-var tunombre;
-var tunombre = prompt("Ingresa tu nombre");
-alert(" Hola Bienvenido:" + tunombre);
+var a = 232;
+
+let b = 120; // let arranca...desde ES6
+
+let myDom = document; // acceso global es el html mismo.
+
+console.log(myDom);
+
+let myapp = document.getElementById("myapp");
+
+myapp.innerHTML = "Estoy modificando mi placeholder";
