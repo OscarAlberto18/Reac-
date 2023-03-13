89f59103bc74222f65ead11bf3efd7e98ab404cf
Oscar <Ibanez0675@gmail.com>
2023-03-13 14:55:18 -0300

    terminado

--- Proyecto Clase 02/Js/app.js
+++ Proyecto Clase 02/Js/app.js
@@ -1,12 +1,4 @@
 
-var a = 232;
+let nombre = prompt('¿Cuál es tu nombre?')
+alert ("Hola:"+ nombre);
 
-let b = 120; // let arranca...desde ES6
-
-let myDom = document; // acceso global es el html mismo.
-
-console.log(myDom);
-
-let myapp = document.getElementById("myapp");
-
-myapp.innerHTML = "Estoy modificando mi placeholder";
